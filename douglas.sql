24c774271ecb36d6e79a98b6417fb95f8c577eef
meveridge <mark.ev@gmail.com>
2014-05-30 20:25:12 -0700

    More DB Changes

--- douglas.sql
+++ douglas.sql
@@ -4,35 +4,6 @@ CREATE DATABASE `douglas` /*!40100 DEFAULT CHARACTER SET latin1 */$$
 
 delimiter $$
 
-CREATE TABLE `douglas`.`article_content` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `article_id` int(11) DEFAULT NULL,
-  `content_id` int(11) DEFAULT NULL,
-  `ordinal` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `article_content_article_id_idx` (`article_id`),
-  KEY `article_content_content_id_idx` (`content_id`),
-  CONSTRAINT `article_content_article_id` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `article_content_content_id` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
-
-
-delimiter $$
-
-CREATE TABLE `douglas`.`article_metadata` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `article_id` int(11) DEFAULT NULL,
-  `metadata_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `article_metadata_article_id_idx` (`article_id`),
-  KEY `article_metadata_metadata_id_idx` (`metadata_id`),
-  CONSTRAINT `article_metadata_article_id` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `article_metadata_metadata_id` FOREIGN KEY (`metadata_id`) REFERENCES `metadata` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=146175 DEFAULT CHARSET=latin1$$
-
-
-delimiter $$
-
 CREATE TABLE `douglas`.`articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '\\''title\\''\\n\\''template\\''\\n\\''dateModified\\''\\n\\''css\\''\\n\\''bodyclass\\''\\n\\''widgets\\''\\n',
   `parent_id` int(11) DEFAULT NULL,
@@ -69,4 +40,30 @@ CREATE TABLE `douglas`.`metadata` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=153446 DEFAULT CHARSET=latin1$$
 
+delimiter $$
 
+CREATE TABLE `douglas`.`article_content` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) DEFAULT NULL,
+  `content_id` int(11) DEFAULT NULL,
+  `ordinal` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `article_content_article_id_idx` (`article_id`),
+  KEY `article_content_content_id_idx` (`content_id`),
+  CONSTRAINT `article_content_article_id` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `article_content_content_id` FOREIGN KEY (`content_id`) REFERENCES `content` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+
+
+delimiter $$
+
+CREATE TABLE `douglas`.`article_metadata` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `article_id` int(11) DEFAULT NULL,
+  `metadata_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `article_metadata_article_id_idx` (`article_id`),
+  KEY `article_metadata_metadata_id_idx` (`metadata_id`),
+  CONSTRAINT `article_metadata_article_id` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `article_metadata_metadata_id` FOREIGN KEY (`metadata_id`) REFERENCES `metadata` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=146175 DEFAULT CHARSET=latin1$$
