cc65b02afe5dc1f669cefae7e1c340b52897db9e
Jay Smith <js@jayasmith.com>
2014-06-19 11:06:47 -0500

    updated the User migration to have an admin and regular user

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -7,12 +7,12 @@ class UserTableSeeder extends Seeder {
         DB::table('users')->delete();
 
         User::create(array('username' => 'rshackleford',
-                'full name' => 'Rusty Shackleford',
+                'fullname' => 'Rusty Shackleford',
                 'email' => 'rshackleford@example.coop',
                 'password' => Hash::make('my_pass'),
                 'isAdmin' => '0'));
         User::create(array('username' => 'bstrickland',
-                'full name' => 'Buck Strickland',
+                'fullname' => 'Buck Strickland',
                 'email' => 'bstrickland@example.coop',
                 'password' => Hash::make('sup_pass'),
                 'isAdmin' => '1'));
